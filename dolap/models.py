62e36209b8be2c3806757340349ae900319815f9
osa1 <omeragacan@gmail.com>
2012-03-29 02:45:42 +0300

    unicode representations of models

--- dolap/models.py
+++ dolap/models.py
@@ -5,8 +5,14 @@ class Shelf(models.Model):
     parent = models.ForeignKey('self', null=True)
     #subshelves = models.ManyToManyField('self')
 
+    def __unicode__(self):
+        return "name=%s, parent=%s" % (self.name, self.parent)
+
 class User(models.Model):
     uid = models.IntegerField(primary_key=True)
+
+    def __unicode__(self):
+        return "uid=%s" % self.uid
 
 class File(models.Model):
     path = models.CharField(max_length=200)
@@ -19,3 +25,6 @@ class File(models.Model):
     mime_type = models.CharField(max_length=100)
     owner = models.ForeignKey(User)
     description = models.TextField(null=True)
+
+    def __unicode__(self):
+        return "path=%s, owner=%s" % (self.path, self.owner.__unicode__())
